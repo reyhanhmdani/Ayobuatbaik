9422aa538e3c4dd011cd046905293d27658b2bac
reyhanhmdani <rey7dan7@gmail.com>
2025-11-28 16:55:33 +0700

    link header Abbi

--- resources/views/components/layout/header.blade.php
+++ resources/views/components/layout/header.blade.php
@@ -1,9 +1,7 @@
 <nav class="mobile-top-bar bg-primary">
     <div class="flex justify-center items-center h-full">
         <a href="{{ route('home') }}">
-            <h1 class="text-base font-bold text-white">
-                <span class="text-secondary">Ayo</span>buatbaik
-            </h1>
+            <h1 class="text-xl font-bold text-white"><span class="text-secondary">Ayo</span>buatbaik</h1>
         </a>
     </div>
 </nav>
