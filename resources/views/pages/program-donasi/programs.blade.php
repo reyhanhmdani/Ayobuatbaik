86b77e1a41a67b7dddf1e64c3ff84cd60de49e7f
reyhanhmdani <rey7dan7@gmail.com>
2025-12-04 16:42:11 +0700

    fix title program

--- resources/views/pages/program-donasi/programs.blade.php
+++ resources/views/pages/program-donasi/programs.blade.php
@@ -81,8 +81,19 @@
 
                     <div class="py-2 px-3 flex flex-col justify-between w-full">
                         <div>
-                            <h3 class="text-xs font-bold text-primary leading-snug line-clamp-2">
-                                {{ $program->title }}
+                            <h3 class="text-xs font-semibold text-gray-800 flex items-center gap-1">
+                                {{ strlen($program->title) > 46 ? substr($program->title, 0, 46) . '...' : $program->title }}
+                                @if ($program->verified)
+                                    <span class="ml-1 inline-flex items-center">
+                                        {{-- SVG Icon Verifikasi ala Instagram --}}
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                            class="w-4 h-4 text-blue-500">
+                                            <path fill-rule="evenodd"
+                                                d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
+                                                clip-rule="evenodd" />
+                                        </svg>
+                                    </span>
+                                @endif
                             </h3>
                             <p class="text-gray-600 text-[10px] mt-0.5 line-clamp-1">
                                 {{ $program->short_description }}
