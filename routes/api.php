724f26bb9363f818af5cdfef2869c22fe69382eb
reyhanhmdani <rey7dan7@gmail.com>
2025-12-10 11:48:37 +0700

    fix stats user

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,18 @@
 <?php
 
+namespace App\Http\Controllers;
+
 use App\Http\Controllers\DonasiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::get("/user", function (Request $request) {
     return $request->user();
-})->middleware('auth:api');
+})->middleware("auth:api");
 
-// Add rate limiting
-Route::middleware('throttle:60,1')->group(function () {
-    Route::post('/donation/{programDonasiId}', [DonasiController::class, 'store']);
+// Donation route dengan web middleware untuk session support
+Route::middleware(["web", "throttle:60,1"])->group(function () {
+    Route::post("/donation/{programDonasiId}", [DonasiController::class, "store"]);
 });
-Route::post('/midtrans/notification', [DonasiController::class, 'notification']);
+
+Route::post("/midtrans/notification", [DonasiController::class, "notification"]);
